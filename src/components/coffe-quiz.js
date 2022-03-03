12a8619c15484ec18af81637122abb99c47fb891
Ian Holmes <iqholmes@gmail.com>
2022-03-03 17:09:36 -0500

    map locates and zooms based on search result

--- src/components/coffe-quiz.js
+++ src/components/coffe-quiz.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const CoffeeQuiz = () => {
@@ -11,104 +11,107 @@ const CoffeeQuiz = () => {
   return (
     <div>
       <div className="CoffeeQuiz">
-        <Form.Group controlId="formBasicSelect">
-          <Form.Label>
-            <h6 className="text-dark">How Strong Do You Like It?</h6>
-          </Form.Label>
-          <Form.Control
-            as="select"
-            value={strength}
-            onChange={(e) => {
-              setStrength(e.target.value);
-            }}
-          >
-            <option value="default">Choose One...</option>
-            <option value="1">Light and aromatic</option>
-            <option value="2">Smooth and balanced</option>
-            <option value="3">Bold and roasty</option>
-            <option value="3">Kill my tastebuds</option>
-          </Form.Control>
+        <Form>
+          <Form.Group controlId="formBasicSelect">
+            <Form.Label>
+              <h6 className="text-dark">How Strong Do You Like It?</h6>
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={strength}
+              onChange={(e) => {
+                setStrength(e.target.value);
+              }}
+            >
+              <option value="default">Choose One...</option>
+              <option value="1">Light and aromatic</option>
+              <option value="2">Smooth and balanced</option>
+              <option value="3">Bold and roasty</option>
+              <option value="3">Kill my tastebuds</option>
+            </Form.Control>
 
-          <br />
+            <br />
 
-          <Form.Label>
-            <h6 className="text-dark">Which Flavors Appeal To You?</h6>
-          </Form.Label>
-          <Form.Control
-            as="select"
-            value={flavors}
-            onChange={(e) => {
-              setFlavors(e.target.value);
-            }}
-          >
-            <option value="default">Choose One...</option>
-            <option value="1">Citrus and mineral</option>
-            <option value="2">Stonefruit and earth</option>
-            <option value="3">Berry and chocolate</option>
-            <option value="3">Caramel and roasted nuts</option>
-          </Form.Control>
+            <Form.Label>
+              <h6 className="text-dark">Which Flavors Appeal To You?</h6>
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={flavors}
+              onChange={(e) => {
+                setFlavors(e.target.value);
+              }}
+            >
+              <option value="default">Choose One...</option>
+              <option value="1">Citrus and mineral</option>
+              <option value="2">Stonefruit and earth</option>
+              <option value="3">Berry and chocolate</option>
+              <option value="3">Caramel and roasted nuts</option>
+            </Form.Control>
 
-          <br />
+            <br />
 
-          <Form.Label>
-            <h6 className="text-dark">Hot Or Cold?</h6>
-          </Form.Label>
-          <Form.Control
-            as="select"
-            value={temp}
-            onChange={(e) => {
-              setTemp(e.target.value);
-            }}
-          >
-            <option value="default">Choose One...</option>
-            <option value="1">Hot</option>
-            <option value="2">Cold</option>
-          </Form.Control>
+            <Form.Label>
+              <h6 className="text-dark">Hot Or Cold?</h6>
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={temp}
+              onChange={(e) => {
+                setTemp(e.target.value);
+              }}
+            >
+              <option value="default">Choose One...</option>
+              <option value="1">Hot</option>
+              <option value="2">Cold</option>
+            </Form.Control>
 
-          <br />
+            <br />
 
-          <Form.Label>
-            <h6 className="text-dark">How Quick Do You Want It?</h6>
-          </Form.Label>
-          <Form.Control
-            as="select"
-            value={speed}
-            onChange={(e) => {
-              setSpeed(e.target.value);
-            }}
-          >
-            <option value="default">Choose One...</option>
-            <option value="1">Yesterday</option>
-            <option value="2">I don't mind waiting a bit</option>
-            <option value="3">I want to savor the process</option>
-          </Form.Control>
+            <Form.Label>
+              <h6 className="text-dark">How Quick Do You Want It?</h6>
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={speed}
+              onChange={(e) => {
+                setSpeed(e.target.value);
+              }}
+            >
+              <option value="default">Choose One...</option>
+              <option value="1">Yesterday</option>
+              <option value="2">I don't mind waiting a bit</option>
+              <option value="3">I want to savor the process</option>
+            </Form.Control>
 
-          <br />
+            <br />
 
-          <Form.Label>
-            <h6 className="text-dark">
-              How Experienced Are You With Pour-over Methods?
-            </h6>
-          </Form.Label>
-          <Form.Control
-            as="select"
-            value={experience}
-            onChange={(e) => {
-              setExperience(e.target.value);
-            }}
-          >
-            <option value="default">Choose One...</option>
-            <option value="1">Trying it out</option>
-            <option value="2">Some. Looking for better results</option>
-            <option value="3">Confident. Expirementing with recipes.</option>
-          </Form.Control>
+            <Form.Label>
+              <h6 className="text-dark">
+                How Experienced Are You With Pour-over Methods?
+              </h6>
+            </Form.Label>
+            <Form.Control
+              as="select"
+              value={experience}
+              onChange={(e) => {
+                setExperience(e.target.value);
+              }}
+            >
+              <option value="default">Choose One...</option>
+              <option value="1">Trying it out</option>
+              <option value="2">Some. Looking for better results</option>
+              <option value="3">Confident. Expirementing with recipes.</option>
+            </Form.Control>
 
-          <br />
-
-          <button type="button" className="btn btn-warning add-post">
-            Get Recipe <i className="fa fa-coffee" />
-          </button>
-        </Form.Group>
+            <br />
+          </Form.Group>
+          <Link to="recipe/1">
+            <Button type="submit" variant="primary">
+              Get Recipe <i className="fa fa-coffee" />
+            </Button>
+          </Link>
+        </Form>
         <Link to="/">back</Link>
       </div>
     </div>
