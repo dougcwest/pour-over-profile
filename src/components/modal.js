ce1e0eae9e98019dbf7e89b5c48d0a62cc3da784
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-03-04 14:31:35 -0600

    removed save changes button

--- src/components/modal.js
+++ src/components/modal.js
@@ -40,9 +40,6 @@ const NewModal = (props) => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
         </Modal.Footer>
       </Modal>
     </>
